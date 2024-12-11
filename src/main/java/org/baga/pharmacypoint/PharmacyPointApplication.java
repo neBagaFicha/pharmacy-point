646de9a113487018001dfeb9896997f0ebe78652
neBagaFicha <sbagrat38@gmail.com>
2024-12-11 22:48:06 +0300

    Initial commit

---
+++ src/main/java/org/baga/pharmacypoint/PharmacyPointApplication.java
@@ -0,0 +1,13 @@
+package org.baga.pharmacypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyPointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmacyPointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/baga/pharmacypoint/PharmacyPointApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baga.pharmacypoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyPointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
