373d34ec7610f970a231d525ff2ee96fefa2cc63
neBagaFicha <sbagrat38@gmail.com>
2024-12-13 14:20:00 +0300

    Добавил простую валидацию

--- src/main/java/org/baga/pharmacypoint/models/Client.java
+++ src/main/java/org/baga/pharmacypoint/models/Client.java
@@ -1,6 +1,9 @@
 package org.baga.pharmacypoint.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.ToString;
 
@@ -16,21 +19,31 @@ public class Client {
     private Integer id;
 
     @Column(name = "Фамилия_клиента")
+    @NotEmpty(message = "Фамилия не может быть пустой")
+    @Size(max = 50, message = "Фамилия не может быть длиннее 50 символов")
     private String clientSurname;
 
     @Column(name = "Имя_клиента")
+    @NotEmpty(message = "Имя не может быть пустым")
+    @Size(max = 50, message = "Имя не может быть длиннее 50 символов")
     private String clientName;
 
     @Column(name = "Отчество_клиента")
+    @Size(max = 50, message = "Отчество не может быть длиннее 50 символов")
     private String clientPatronymic;
 
     @Column(name = "Номер_телефона_клиента")
+    @NotEmpty(message = "Номер телефона не может быть пустым")
+    @Size(max = 15, message = "Номер телефона не может быть длиннее 15 символов")
     private String clientPhone;
 
     @Column(name = "Адрес_электронной_почты_клиента")
+    @NotEmpty(message = "Адрес электронной почты не может быть пустым")
+    @Email(message = "Некорректный адрес электронной почты")
     private String clientEmail;
 
     @Column(name = "Предпочтения")
+    @Size(max = 255, message = "Предпочтения не могут быть длиннее 255 символов")
     private String clientPreferences;
 
     @OneToMany(mappedBy = "client")
